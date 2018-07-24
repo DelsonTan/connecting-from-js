f094cbc4bc5b80bb45d274ff065bcb875222e6df
Delson Tan <delsonwtan@gmail.com>
2018-07-24 20:09:18 +0000

    Add lookup_people.js that searches the database for the details of a name being passed as the argument in the command line when running node lookup_people.js

---
+++ lookup_people.js
@@ -0,0 +1,38 @@
+const pg = require("pg");
+const settings = require("./settings"); // settings.json
+const arg = process.argv[2];
+
+const client = new pg.Client({
+  user     : settings.user,
+  password : settings.password,
+  database : settings.database,
+  host     : settings.hostname,
+  port     : settings.port,
+  ssl      : settings.ssl
+});
+
+function runQuery(query, values, cb) {
+    client.query(query, values, (err, result) => {
+        if (err) {
+            return console.error("error running query", err);
+        }
+        cb(result);
+        client.end();    
+    });
+}
+
+client.connect((err) => {
+  if (err) {
+    return console.error("Connection Error", err);
+  }
+  const query = "SELECT first_name, last_name, TO_CHAR(birthdate, 'yyyy-mm-dd') AS birthdate FROM famous_people WHERE first_name = $1::text";
+  
+  runQuery(query, [arg], (data) => {
+    console.log("Searching ...");
+    console.log(`Found ${data.rowCount} person(s) by the name '${arg}':`)
+    data.rows.forEach((person, index) => {
+        console.log(`- ${index + 1}: ${person.first_name} ${person.last_name}, born ${person.birthdate}`); 
+    });
+    client.end();
+  });
+});
